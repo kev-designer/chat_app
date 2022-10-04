d36b2113ac0d6c2241c1bbfda6c3686a2c7f878b
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-10-04 11:52:51 +0530

    4th oct

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:chat_app/view/sign_in.dart';
 import 'package:chat_app/widget/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
