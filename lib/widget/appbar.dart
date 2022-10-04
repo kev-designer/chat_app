654862ac71939988e4466ac2e9a62656eb9a998e
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-10-04 07:42:21 +0530

    4th oct

--- lib/widget/appbar.dart
+++ lib/widget/appbar.dart
@@ -1,4 +1,3 @@
-//BACK ARROW ICON
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
@@ -7,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 import 'const.dart';
 
+//BACK ARROW ICON
 AppBar buildAppBar(
   BuildContext context,
   String appbarTitle,
@@ -28,6 +28,44 @@ AppBar buildAppBar(
         ),
       ),
     ),
+    title: Text(
+      appbarTitle,
+      style: GoogleFonts.nunito(
+        fontSize: height(context) * .025,
+        fontWeight: FontWeight.w800,
+        color: ColorData.black,
+      ),
+    ),
+  );
+}
+
+//HAMBURGURE MENU & SEARCH ICON
+AppBar buildMessageAppBar(
+  BuildContext context,
+  String appbarTitle,
+) {
+  return AppBar(
+    centerTitle: true,
+    elevation: 1,
+    shadowColor: ColorData.black.withOpacity(0.1),
+    backgroundColor: Colors.white,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                HapticFeedback.heavyImpact();
+              },
+              child: SvgPicture.asset(
+                "assets/svg/32/search.svg",
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
     title: Text(
       appbarTitle,
       style: GoogleFonts.nunito(
